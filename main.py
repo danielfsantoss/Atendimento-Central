253a3a42585aa306b891e58db3a20e40f222fe63
Daniel Figueiredo <58513369+wudashii@users.noreply.github.com>
2022-10-04 17:46:39 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+menu_repeticao = 1
+escolha_menu = ""
+continua_menu = "Y"
+quebra_linha = "="*30
+
 class Atendimento:
     def __init__(self, nome, empresa, contato, resumo):
         self.nome = nome_cliente
@@ -6,43 +11,44 @@ class Atendimento:
         self.resumo = resumo_atendimento
     
 
-def confirma_escolha():
+def confirma_escolha(): # Erro semântico
     continua_menu = input("Deseja continuar? [Y/N]: ").upper()
-    while True:
-        if(continua_menu == "Y"):
-            menu_repeticao = 1
-            break
-        elif(continua_menu == "N"):
-            print("ROTINA ENCERRADA!")
-            arquivo.close() 
-            break
-        else:
-            continua_menu = input("Digite 'Y' ou 'N': ").upper()
+    if(continua_menu == "N"):
+        print("ROTINA ENCERRADA!")
+        global menu_repeticao
+        menu_repeticao = 0
+    return 0
 
 
-menu_repeticao = 1
-escolha_menu = ""
-continua_menu = "Y"
-quebra_linha = "="*30
+def gerar_txt():
+    arquivo = open("Atendimentos.txt", "a")
+    arquivo.writelines(f"NOME: {atendimento.nome}\nEMPRESA: {atendimento.empresa}\nCONTATO: {atendimento.contato}\nRESUMO: {atendimento.resumo}\n{quebra_linha}\n")
+    arquivo.close()
+    return 0
+
+
+def gerar_csv():
+    return 0
+
 
 while True:
     escolha_menu = int(input("1. Incluir Registros\n2. Consultar Atendimentos\n3. Sair\nEscolha: "))
     if(escolha_menu == 1):
         while menu_repeticao == 1:
-            arquivo = open("Atendimentos.py", "a")
             nome_cliente = input("NOME DO CLIENTE: ").title()
             nome_empresa = input("NOME DA EMPRESA: ").title()
             contato_cliente = int(input("NUMERO DO CLIENTE: "))
             resumo_atendimento = input("RESUMO DO ATENDIMENTO: ").title()
             atendimento = Atendimento(nome_cliente, nome_empresa, contato_cliente, resumo_atendimento)
-            arquivo.writelines(f"NOME: {atendimento.nome}\nEMPRESA: {atendimento.empresa}\nCONTATO: {atendimento.contato}\nRESUMO: {atendimento.resumo}\n{quebra_linha}\n")
-            arquivo.close()
             print("REGISTRO INCLUIDO!")
             confirma_escolha()
     elif(escolha_menu==2):
-        arquivo = open("Atendimentos.py", "r")
+        formato_arquivo = int(input(""))
+        """
+        arquivo = open("Atendimentos.txt", "r")
         print(arquivo.readlines())
         confirma_escolha()
+        """ # GERAR TXT DIRETO
     elif(escolha_menu==3):
         break
 # Segue
